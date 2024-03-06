021e0598984753bb3216f53638aef54894a3690b
Feridun <feridun@aerlabs.com>
2024-03-06 13:38:22 +0100

    created a new folder for the navigation sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "../styles/Navbar.scss";
+import Sidebar from "./sidebar/Sidebar";
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <div className="wrapper">
-        <span className="navbar-details">Isim</span>
-        <span className="navbar-details">Tel no</span>
+    <div className="navbar-wrapper">
+      <div className="navbar">
+        <div className="navbar-details-wrapper">
+          <span className="navbar-details">Isim</span>
+          <span className="navbar-details">Tel no</span>
+        </div>
+        <Sidebar />
       </div>
     </div>
   );
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../styles/Sidebar.scss";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {" "}
+      <div className="menu-background">Links</div>
+    </div>
+  );
+}
+
+export default Sidebar;

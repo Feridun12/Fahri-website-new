656baa2aaee4b9cee05a726766ecd13c8b8418bd
Feridun <feridun@aerlabs.com>
2024-03-08 13:32:05 +0100

    added the contact details to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,10 @@ import "../styles/Navbar.scss";
 const Navbar = () => {
   return (
     <div className="navbar-wrapper">
+      <div className="navbar-details-wrapper">
+        <p className="navbar-detail">Fahri Erbulak</p>
+        <p className="navbar-detail">+90 533 851 9561</p>
+      </div>
       <Sidebar />
     </div>
   );

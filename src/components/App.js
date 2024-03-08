7a8bde9730c416accaa71ac21ede44409c7fc349
Feridun <feridun@aerlabs.com>
2024-03-08 15:05:31 +0100

    created the main components and a system to display them aligned with the navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import Navbar from "./Navbar";
+import Intro from "./Intro";
+import AboutMe from "./AboutMe";
+import GoogleReview from "./GoogleReview";
+import ContactMe from "./ContactMe";
 import "../styles/App.scss";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <section>Home</section>
-      <section>Hakkımızda</section>
-      <section>Sizden gelenler</section>
-      <section>İletişim</section>
+      <div className="sections">
+        <Intro />
+        <AboutMe />
+        <GoogleReview />
+        <ContactMe />
+      </div>
     </div>
   );
 }
---
+++ src/components/GoogleReview.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GoogleReview() {
+  return <div>GoogleReview</div>;
+}
+
+export default GoogleReview;
